bc9777346f42b75771d9812cb8b3c578988d6648
Isuru <izurumw@gmail.com>
2023-11-08 21:30:16 +0530

    java connection

---
+++ javaScript/script.js
@@ -0,0 +1,9 @@
+ console.log("Isuru");
+ console.log("Hello world");
+ console.log('Welcome JavaScript');
+
+//  alert('hi!!!!!!!');
+ document.write('baba');
+
+ document.write('<br>');
+ document.write('<h1>baba1</h1>');
