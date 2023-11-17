e48c892fc31241f0f6aa8cfb80f2247cd712a30b
Isuru <izurumw@gmail.com>
2023-11-17 22:10:06 +0530

    loops

--- javaScript/script.js
+++ javaScript/script.js
@@ -51,16 +51,83 @@ if (true) {
     console.log("false");
 }
 
-const mark = 25;
+const mark = 89;
 
-if (80<mark) {
-    
-    console.log('A');
-    
-}
+if (80 < mark) {
+    if (mark == 90) {
+        console.log("marks=90");
+    } else {
+        console.log ("other");
+    }
+   }
 else if (50<mark){
     console.log("B");
 }
 else {
     console.log("C");
 }
+const swi="AFSD"
+switch (swi) {
+    case "ACPT":
+        console.log("ACPT"); break;
+    case "AFSD":
+        console.log("Print AFSD"); break;
+    default:
+        console.log("Not Known");
+
+
+}
+// loop
+for (let i=0; i<10; i++){
+    console.log(i);    
+}
+const arr = ["A", "B", "C", "D", "E","F", "J", "H","I","J"];
+
+console.log(arr[4]);
+
+for (let o=0; o<arr.length; o++){
+    console.log(arr[o]);
+}
+
+// const w = 0;
+// while (w<10) {
+//     console.log("AFCT");
+//     w++
+// }
+
+let p = 0;
+do {
+    console.log(p);
+    p++
+} while (p < 10);
+
+for (let q = 0; q < 10; q++){
+    console.log(q);
+    if (q == 7) {
+        break;
+    }
+}
+
+const fruits = ["banana", "papaya", "orange", "apple", "mango"];
+
+const fruit = fruits.pop();
+console.log(fruit);
+
+// fruits.push("Kiwi");
+// console.log(fruits);
+
+// const fruitt = fruits.shift();
+console.log(fruits.shift());
+fruits.unshift("grapes");
+console.log(fruits);
+
+// object
+
+const person = {
+    name: "isuruww", addr: "Battaramulla", age: "23",
+    run: function () {console.log("run")},
+    walk : function(){console.log("walk")},
+    }
+const name1 = person.addr
+person.run();
+person.walk();
